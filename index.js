1a03b372bf52631d1323ab18dc08d662ef599ac3
Lara <laraellenh@gmail.com>
2022-01-17 12:31:30 -0500

    commit tree 4d35399ea07da3971cb7a9733a7934b63da1524c

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
+// declare the addingEventListener func, grab the input, add click event with anon callback
+
+
 
 function addingEventListener() {
-    const input = document.getElementById('input');
-    input.addEventListener('click', function() {
-      alert('I was clicked!');
-    });
+  const input = document.getElementById('input');
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 
 }
+
